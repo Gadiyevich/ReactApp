b1acf46c6ab4e5f750a59059024423b9588704c1
gadiyevich <48290063+gadiyevich@users.noreply.github.com>
2020-04-18 01:38:46 +0400

    added

---
+++ build/precache-manifest.1698f85d9d548ca7b7dcc26178b6fa60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556d36f2ae7f93378c8f7516b977b6e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08e1a221c295c95ecf91",
+    "url": "/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "1d5d63c3b57af90bfc17",
+    "url": "/static/css/main.aab8614d.chunk.css"
+  },
+  {
+    "revision": "08e1a221c295c95ecf91",
+    "url": "/static/js/2.29335d3a.chunk.js"
+  },
+  {
+    "revision": "0897a2af4d47251d787cefe2723205c2",
+    "url": "/static/js/2.29335d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d5d63c3b57af90bfc17",
+    "url": "/static/js/main.daf51072.chunk.js"
+  },
+  {
+    "revision": "99fba9a25b14f664f01f",
+    "url": "/static/js/runtime-main.6508bb52.js"
+  }
+]);
